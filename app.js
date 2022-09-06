f1e5fffe16b3c189fbd3bec4e75205217dc027e4
Aman kumar <amankumar12091999@gmail.com>
2022-09-06 20:56:59 +0530

    feat: toFixed added to result

--- app.js
+++ app.js
@@ -17,11 +17,13 @@ function calculateProfitAndLoss(initial,quantity,current){
     if(initial > current){
         var loss = (initial - current)*quantity;
         var lossPercentage  = (loss/initial)*100;
+        lossPercentage = lossPercentage.toFixed(2);
         showOutput(`Hey the loss is ${loss} and the percent is ${lossPercentage}`);
     }
     else if(current> initial){
         var profit = (current - initial)*quantity;
         var profitPercentage = (profit/initial)*100;
+        profitPercentage = profitPercentage.toFixed(2)
         showOutput(`hey the profit is ${profit} and the percent is ${profitPercentage}`);
 
     }
